65d9ab235de46799d398eec7cfb4f5405c35b70e
Victor Liu <victorliu716@gmail.com>
2020-03-10 22:14:14 -0500

    Implement worker threads, benchmark against clustering

--- main/index.js
+++ main/index.js
@@ -2,11 +2,25 @@ const crypto = require('crypto')
 const express = require('express')
 const app = express()
 
+const Worker = require('webworker-threads').Worker;
 
 app.get('/', (req, res) => {
-  crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-    res.send("test msg 123")
-  })    
+
+  const worker = new Worker(function() {
+    this.onmessage = function() {
+      let counter = 0
+      while (counter < 1e9) {
+        counter++
+      }
+      postMessage(counter)
+    }
+  })
+
+  worker.onmessage = function(myCounter) {
+    console.log(myCounter)
+  }
+
+  worker.postMessage(counter)
 })
 
 app.get('/fast', (req, res) => {
